dcaa4c9eb0a817816e40d52c8c79e2b6c80bf556
Eduardo Serrano Luque <eduardo.serrano@iesmartinezm.es>
2023-10-25 11:28:03 +0200

    Creado el form de tablero y renderizado en template

---
+++ ut03/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class CreaTableroForm(forms.Form):
+    filas = forms.IntegerField(label='Filas',min_value=1, max_value=20, required= True, initial=2)
+    columnas = forms.IntegerField(label='Columnas', min_value=1, max_value=20, required=True, initial=2)
--- ut03/views.py
+++ ut03/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .forms import CreaTableroForm
 
 # Create your views here.
 def welcome(request):
     return render(request, 'ut03/index.html', {})
 
 def crea_tablero(request):
-    return render(request, 'ut03/crea_tablero.html', {})
+    tablero_form = CreaTableroForm()
+    return render(request, 'ut03/crea_tablero.html', {'form':tablero_form})
