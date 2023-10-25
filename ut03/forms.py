81967217432e0517c7925dd14126e74036ca3dc9
Eduardo Serrano Luque <eduardo.serrano@iesmartinezm.es>
2023-10-25 12:59:57 +0200

    Solucion completa

--- ut03/forms.py
+++ ut03/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class CreaTableroForm(forms.Form):
     filas = forms.IntegerField(label='Filas',min_value=1, max_value=20, required= True, initial=2)
-    columnas = forms.IntegerField(label='Columnas', min_value=1, max_value=20, required=True, initial=2)
+    columnas = forms.IntegerField(label='Columnas', min_value=1, max_value=15, required=True, initial=2)
--- ut03/views.py
+++ ut03/views.py
@@ -6,5 +6,17 @@ def welcome(request):
     return render(request, 'ut03/index.html', {})
 
 def crea_tablero(request):
+    # Si se ha enviado el formulario
     tablero_form = CreaTableroForm()
+    if request.method == 'GET':
+        tablero_form = CreaTableroForm(request.GET)
+        #Ejecutamos la validacion
+        if tablero_form.is_valid():
+            #Los datos se cogen del diccionario cleaned_data
+            columnas = tablero_form.cleaned_data['columnas']
+            filas = tablero_form.cleaned_data['filas']
+            return render(request, 'ut03/muestra_tablero.html',
+                          {'filas':filas,'columnas':columnas,
+                                    'rango_filas':range(filas), 'rango_columnas': range(columnas)})
+    #Si se pide la página por primera vez
     return render(request, 'ut03/crea_tablero.html', {'form':tablero_form})
